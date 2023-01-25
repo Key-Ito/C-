7c85fe317c34c2b2c59e386b221c7ae25090a3cb
Key <arhiv.baev@gmail.com>
2023-01-25 16:24:33 +0300

    Seminar1

---
+++ Semirar1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write ("Введите первое целое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+
+Console.Write ("Введите второе целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
+
+int userNumber1 = int.Parse(userInput1);
+int userNumber2 = int.Parse(userInput2);
+
+if (userNumber1 == userNumber2 * userNumber2)
+{
+    Console.WriteLine ("a = " + userNumber1 + ", b = " + userNumber2 + " -> да");   
+}
+else
+    {
+    Console.WriteLine ("a = " + userNumber1 + ", b = " + userNumber2 + " -> нет");
+    }
+
+//Console.WriteLine(userNumber + " -> " + userNumber * userNumber);
