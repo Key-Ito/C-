c6eccbe7d78ad08f7bee6494197f479e6410cbf0
Key <arhiv.baev@gmail.com>
2023-02-08 23:45:11 +0300

    S2HW

---
+++ Seminar2/HW/ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿int EnterNumber()
+ {
+    int number;
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        if (int.TryParse(Console.ReadLine(), out number))
+            break;
+        Console.WriteLine("Ошибка ввода!");
+    }
+    return number;
+}
+int g = EnterNumber();
+Console.Clear ();
+
+Console.Write ($"{g} -> ");
+
+if (g < 101)
+    Console.WriteLine ($"третьей цыфры нет");
+Console.WriteLine ()
