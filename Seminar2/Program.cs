49d4085a03a7bcb8420856895ba3bbe8b63f0a94
Key <arhiv.baev@gmail.com>
2023-02-08 21:58:06 +0300

    Цикл+%+/

---
+++ Seminar2/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int EnterNumber()
+        {
+            int number;
+            while (true)
+            {
+                Console.Write("Введите число: ");
+                if (int.TryParse(Console.ReadLine(), out number))
+                    break;
+                Console.WriteLine("Ошибка ввода!");
+            }
+            return number;
+        }
+        int firstNumber = EnterNumber();
+        int secondNumber = EnterNumber();
+
+        Console.Clear();
+        Console.Write($"{firstNumber}, {secondNumber} -> ");
+        if (firstNumber % secondNumber == 0)
+            Console.WriteLine("кратно");
+        else
+            Console.Write($"не кратно, остаток {firstNumber % secondNumber}");
+
+    }
+}
---
+++ Semirar1/HW/Program4.cs
@@ -0,0 +1,26 @@
+﻿
+Console.Write ("Введите первое целое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+
+Console.Write ("Введите второе целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
+
+Console.Write ("Введите тертье целое число: ");
+string userInput3 = Console.ReadLine() ?? "";
+
+int userNumber1 = int.Parse(userInput1);
+int userNumber2 = int.Parse(userInput2);
+int userNumber3 = int.Parse(userInput3);
+
+Console.Write (userNumber1 + ", " + userNumber2 + ", " + userNumber3 + " -> ");
+
+if (userNumber1 > userNumber2)
+{
+    Console.Write (userNumber1);   
+}
+else
+    {
+    Console.Write (userNumber2);
+    }
+
+//Console.WriteLine(userNumber + " -> " + userNumber * userNumber);
