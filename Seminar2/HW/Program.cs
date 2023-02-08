63745493b9c1aedea082be75e8edfddb3f14a4f7
Key <arhiv.baev@gmail.com>
2023-02-08 22:53:53 +0300

    S1HW1

---
+++ Seminar2/HW/Program.cs
@@ -0,0 +1,16 @@
+﻿int EnterNumber()
+ {
+    int number;
+    while (true)
+    {
+        Console.Write("Введите трёхзначное число: ");
+        if (int.TryParse(Console.ReadLine(), out number) && number > 100 && number < 1000)
+            break;
+        Console.WriteLine("Ошибка ввода!");
+    }
+    return number;
+}
+int g = EnterNumber();
+Console.Clear ();
+
+Console.Write ($"{g} -> {(g / 10) % 10 }");
