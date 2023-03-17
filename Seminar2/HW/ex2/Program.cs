ee377599a6fb5f5840587076bfed3085ccd45249
Ito <arhiv.baev@gmail.com>
2023-03-17 21:38:55 +0300

    Задача 2 ДЗ

--- Seminar2/HW/ex2/Program.cs
+++ Seminar2/HW/ex2/Program.cs
@@ -15,6 +15,17 @@ Console.Clear ();
 
 Console.Write ($"{g} -> ");
 
+int r = g;
+
 if (g < 101)
+{
     Console.WriteLine ($"третьей цыфры нет");
-Console.WriteLine ()
+}
+else 
+{
+    while ( r > 1001 )
+    {
+      r =  r / 10;
+    }
+    Console.WriteLine ( $"{r % 10}" );
+}
