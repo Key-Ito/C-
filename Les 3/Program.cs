f0a803f4aa879d9b0d403f5d50b83e031a8ad03f
Ito <arhiv.baev@gmail.com>
2023-03-19 19:02:04 +0300

    Урок 4 вида Методов

--- Les 3/Program.cs
+++ Les 3/Program.cs
@@ -1,4 +1,4 @@
-﻿// Методы
+﻿// Методы ВИд1 МЕТОДЫ НИЧЕГО НЕ ВОЗВРАЩАЮТ И НИЧЕГО НЕ ПРИНИМАЮТ
 void Method1 ()
 {
     Console.WriteLine ("Автор Артур");
@@ -6,8 +6,7 @@ void Method1 ()
 //Method1 ();
 
 
-
-// Метод 2
+// Метод 2 ВИД 2 МЕТОДЫ НИЧЕГО НЕ ВОЗВРАЩАЮТ И НО МОГУТ ПРИНИМАТЬ ИНФОРМАЦИЮ
 void Method2 (string msg)
 {
     Console.WriteLine (msg);
@@ -23,4 +22,32 @@ void Method21 (string msg, int count   )
         i ++; 
     }
 }
-Method21 ("Текст", 4);
+//Method21 (msg: "Текст", count: 4);
+//Method21 (msg: "New Текст", count: 4);
+
+
+// Метод 3 ВИД 3 МЕТОДЫ ЧТО-ТО  ВОЗВРАЩАЮТ И НО НИЧЕГО НЕ ПРИНИМАЮТ
+int Method3 ()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3 ();
+//Console.WriteLine(year);
+
+
+// Метод 4 ВИД 4 МЕТОДЫ ЧТО-ТО  ВОЗВРАЩАЮТ И что-то ПРИНИМАЮТ
+string Method4 (int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+        while (i < count)
+        {
+            result = result + text;
+            i++;
+        }
+        return result;
+}
+ 
+string res =  Method4 (10 , "Текст");
+//Console.WriteLine (res);
