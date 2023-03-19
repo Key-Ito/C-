4258beb09880d8e672b4101ecfe6515551c319ad
Ito <arhiv.baev@gmail.com>
2023-03-19 18:34:02 +0300

    Урок 3 Методы

---
+++ Les 3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Методы
+void Method1 ()
+{
+    Console.WriteLine ("Автор Артур");
+}
+//Method1 ();
+
+
+
+// Метод 2
+void Method2 (string msg)
+{
+    Console.WriteLine (msg);
+}
+//Method2 (msg: "Текст сообщения");
+
+void Method21 (string msg, int count   )    
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine (msg);
+        i ++; 
+    }
+}
+Method21 ("Текст", 4);
