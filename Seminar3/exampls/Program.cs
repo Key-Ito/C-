fece2ac34da878e44edc26368b48dd76477d94b0
Ito <arhiv.baev@gmail.com>
2023-03-21 16:14:00 +0300

    Решение задачи 22 таблица квадратов

--- Seminar3/exampls/Program.cs
+++ Seminar3/exampls/Program.cs
@@ -1,4 +1,5 @@
-﻿// задача 21 
+﻿/*
+//                                                              задача 21 
 // Принимает координаты двух точек и находит рассотяние между ними в 2D простанстве
 // A (3 , 6) B (2 ,1)
 
@@ -23,4 +24,26 @@ static double Distanse (int a1, int a2,int b1, int b2)
     double result = Math.Sqrt(Math.Pow(b1 - a1, 2) + Math.Pow(b2-a2, 2));
     // Math.Sqrt - корень из b1 -a1 (разности чисел в нашем случае), 2- значит квадратный корень
     return result;
+}
+*/
+
+
+//                                  Задача 22
+// Написать прогу, которая принимает на вход число (N) 
+// и выдает таблицу квадратов от чисел 1 до N.
+
+Console.Write ("Введите число N: ");
+int N = int.Parse(Console.ReadLine()?? "");
+
+Console.Write ($"{N} -> ");
+PrintSquares(N); 
+
+void PrintSquares(int N)
+{
+    for (int i = 1; i < N; i++)
+    {
+        int result = i * i;
+        Console.Write ($"{result}, ");
+    }
+    Console.Write ($"{N * N}");
 }
