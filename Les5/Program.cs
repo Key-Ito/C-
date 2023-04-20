c4ec2b2e61bda675e78813d277fbc8e9b90c8390
Ito <arhiv.baev@gmail.com>
2023-04-20 18:32:02 +0300

    Les5

--- Les5/Program.cs
+++ Les5/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//                      как нужно писать код
+/*
+int[] CreatArray (int N){ // Массив который принимает число N и создает массив от числа -N до N.
+    int[] array = new int[N * 2 + 1];
+    for (int i = -N; i <= N; i++){
+        array[i + N] = i;
+    }
+    return array;}
+
+
+
+Random r = new Random();
+
+Console.CursorVisible = false;
+while (true){
+    Console.SetCursorPosition(
+        left: r.Next(Console.WindowWidth),
+        top: r.Next(Console.WindowWidth)
+        );
+    Console.Write (r.Next(10));
+    Thread.Sleep(1000);
+}
+*/
+
